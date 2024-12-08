2091b87d1ac6dc8338fa761d91bd1641c12d47a5
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2024-12-09 00:46:07 +0200

    Submit: 0x01-react_intro --> task_2

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import {getFooterCopy, getFullYear} from './utils';
+import './Notifications.css';
+
+export default function Notifications() {
+    return (
+        <div className="Notifications">
+            <p>Here is the list of notifications</p>
+        </div>
+    )
+}
