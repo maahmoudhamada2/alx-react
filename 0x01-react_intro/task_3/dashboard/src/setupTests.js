2837bcfc7a88cf1b0f1315190f6e44bbbaebb67e
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2024-12-09 01:55:35 +0200

    Fixing: 0x01-react_intro --> task_3

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
-import { configure } from 'enzyme';
-import { Adapter } from '@cfaester/enzyme-adapter-react-18';
-
-configure({Adapter: new Adapter()})
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
